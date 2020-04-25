ef9b55f76c5b2b74dce60a17f21bd41ff706ea8d
xdc <1434412784@qq.com>
2020-04-25 23:55:29 +0800

    feat: 完成充值页面模块

--- pages/recharge/recharge.js
+++ pages/recharge/recharge.js
@@ -1,18 +1,48 @@
 // pages/recharge/recharge.js
+const util = require('../../utils/util.js')
+const {http} = require('../../utils/http.js')
+
 Page({
+
+  rechargeAccount: 0,
 
   /**
    * 页面的初始数据
    */
   data: {
+    account: 0
+  },
+  watchAmount(event) {
+    this.rechargeAccount = event.detail.value
+  },
+  toRecharge() {
 
+    if (this.rechargeAccount == 0) {
+      wx.showToast({
+        title: '请输入大于 0 的金额',
+        icon: 'none',
+        duration: 2000
+      })
+
+      return;
+    }
+
+    http.post(`/recharge`, {
+      rechargeAccount: this.rechargeAccount
+    }).then(response => {
+      wx.setStorageSync('me', JSON.parse(response.data).data)
+
+      wx.navigateBack()
+    })
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    wx.setNavigationBarTitle({
+      title: '我的账户'
+    })
   },
 
   /**
@@ -26,7 +56,9 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
+    this.setData({
+      account: util.getAccount()
+    })
   },
 
   /**
