f13c0287403547b291b78d7bd385fca376534070
xdc <1434412784@qq.com>
2020-04-28 22:08:02 +0800

    feat: 完成展示商铺列表

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,8 +1,28 @@
-
+const {http} = require("../../utils/http")
 
 Page({
   data: {
-    storeList: []
+    storeList: [],
+    areaId: "441928",
+    areaName: "寮步镇"
   },
 
+  onLoad() {
+    // 发起请求，先把寮步镇的所有的店铺请求回来
+    // 展示出店铺
+
+    http.get(`/shop/area/${this.data.areaId}`).then(response => {
+      this.setData({
+        storeList: JSON.parse(response.data)["data"]
+      })
+    })
+  },
+
+  selectShop(event) {
+    const id = event.detail
+
+    http.get(`/shop/id/${id}`).then(response => {
+      console.dir(response)
+    })
+  }
 })
