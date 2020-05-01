df2eca9b2eedb711b7d279935405bc3de9e43dc7
xdc <1434412784@qq.com>
2020-05-01 23:54:51 +0800

    feat: 完成订单列表的信息展示，还剩取消订单功能

--- pages/order/list/list.js
+++ pages/order/list/list.js
@@ -1,66 +1,26 @@
 // pages/order/list/list.js
+const {http} = require("../../../utils/http")
+
 Page({
 
-  /**
-   * 页面的初始数据
-   */
   data: {
+    orderList: []
+  },
+  cancelOrder(event) {
 
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  onShow() {
+    http.get("/order").then(response => {
+      this.setData({
+        orderList: JSON.parse(response.data)["data"]
+      })
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  callShopOwen(event) {
+    wx.makePhoneCall({
+      phoneNumber: event.target.dataset.phone
+    })
   }
 })
