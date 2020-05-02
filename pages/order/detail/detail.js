9d54bb7630044189ca34754b6334462e0e12d705
xdc <1434412784@qq.com>
2020-05-02 10:10:50 +0800

    feat: 完成订单详情页

--- pages/order/detail/detail.js
+++ pages/order/detail/detail.js
@@ -1,66 +1,21 @@
-// pages/order/detail/detail.js
+const {http} = require("../../../utils/http")
+const {formatTime} = require("../../../utils/util")
+
 Page({
 
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    order: {}
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
+  onLoad(event) {
+    http.get(`/order/${event.id}`).then(response => {
+      const order = JSON.parse(response.data)["data"]
 
-  },
+      order.createdAt = formatTime(new Date(order.createdAt))
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+      this.setData({
+        order
+      })
+    })
   }
 })
