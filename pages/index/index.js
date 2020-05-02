6a8d21c99fc9559a7e3130dd19df4301550cf70d
xdc <1434412784@qq.com>
2020-05-02 18:29:59 +0800

    feat: 完成首页的地区手动选择

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,8 +1,13 @@
 const {http} = require("../../utils/http")
 
 Page({
+  areaList: [],
+
   data: {
     storeList: [],
+
+    areaNameList: [],
+    areaIndex: 3,
     areaId: "441928",
     areaName: "寮步镇"
   },
@@ -10,7 +15,23 @@ Page({
   onLoad() {
     // 发起请求，先把寮步镇的所有的店铺请求回来
     // 展示出店铺
+    http.get("/address/areaList").then(response => {
+      this.areaList = JSON.parse(response.data)["data"]
+      const areaNameList = []
 
+      this.areaList.forEach(area => {
+        areaNameList.push(area.name)
+      })
+
+      this.setData({
+        areaNameList
+      })
+    })
+
+    this.getShopList()
+  },
+
+  getShopList() {
     http.get(`/shop/area/${this.data.areaId}`).then(response => {
       this.setData({
         storeList: JSON.parse(response.data)["data"]
@@ -24,5 +45,17 @@ Page({
     wx.navigateTo({
       url: "/pages/shop/shop?id=" + id
     })
+  },
+
+  bindPickerAreaChange(event) {
+    let areaIndex = event.detail.value
+
+    this.setData({
+      areaIndex,
+      areaName: this.areaList[areaIndex].name,
+      areaId: this.areaList[areaIndex].id
+    })
+
+    this.getShopList()
   }
 })
