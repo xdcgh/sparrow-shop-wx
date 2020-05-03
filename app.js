4d44df899cd8042f50a46a05f547f3ecc5015bc9
xdc <1434412784@qq.com>
2020-05-03 09:44:58 +0800

    feat: 上线准备

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 //app.js
 App({
   globalData: {
-    host: 'http://192.168.31.15:8080/api/wx',
+    // host: 'http://192.168.31.15:8080/api/wx',
+    host: 'http://xdc.myds.me:9080/api/wx',
     userInfo: wx.getStorageSync("me")
+  },
+  onLaunch(options) {
+    // wx.setEnableDebug({
+    //   enableDebug: true
+    // })
   }
 })
