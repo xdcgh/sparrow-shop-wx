8e904ff84dd73c19ba66095c870a675968bfe85d
xdc <1434412784@qq.com>
2020-05-03 18:29:36 +0800

    feat: 添加提示，订单页为空时

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 //app.js
 App({
   globalData: {
-    // host: 'http://192.168.31.15:8080/api/wx',
-    host: 'http://xdc.myds.me:9080/api/wx',
+    host: 'http://192.168.31.15:8080/api/wx',
+    // host: 'http://xdc.myds.me:8080/api/wx',
+    // host: 'http://xdc.myds.me:9080/api/wx',
+    // host: 'https://panbyte.cn:9443/api/wx',
     userInfo: wx.getStorageSync("me")
   },
   onLaunch(options) {
